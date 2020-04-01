80dd8e4e3750214a2f90f05228914c067bd08990
Abo Lilagit config --global user.email b0lilaaa7@gmail.com <49341933+bolilaaa@users.noreply.github.com>
2020-04-01 09:33:11 +0200

    Edit Orders function and datamembers
    Co-Authored-By: Bassam Fotouh <bfotouh@users.noreply.github.com>
    Co-Authored-By: monemali <monemali@users.noreply.github.com>

--- Restaurant/Rest/Order.cpp
+++ Restaurant/Rest/Order.cpp
@@ -7,25 +7,58 @@ Order::Order(int id, ORD_TYPE r_Type)
 	status = WAIT;
 }
 
-Order::~Order()
+Order::Order(int id, ORD_TYPE r_Type, double pir) {
+	ID = id;
+	type = r_Type;
+	status = WAIT;
+	priority = pir;
+}//constructor for VIP orders
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//Setters // 
+void Order::setStatus(ORD_STATUS s)
 {
+	status = s;
+}
+void Order::SetDistance(int d)
+{
+	Distance = d > 0 ? d : 0;
 }
 
+void Order::Setpriority(double r) {
+	priority = r;
+}
+void Order::setWaitTime(int time) {
+	WaitTime = time;
+}
+void Order::setMoney(double Cost) {
+	totalMoney = Cost;
+}
+void Order::setArrivalTime(int Arr) {
+	ArrTime = Arr;
+}
+void Order::setType(ORD_TYPE r_Type) {
+	type = r_Type;
+}
+void Order::setStartSrvTime(int time) {
+	ServTime = time;
+}
+void Order::setFinTime(int time) {
+	FinishTime = time;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Getters //
 int Order::GetID()
 {
 	return ID;
 }
 
-
 ORD_TYPE Order::GetType() const
 {
 	return type;
-}
-
-
-void Order::SetDistance(int d)
-{
-	Distance = d>0?d:0;
 }
 
 int Order::GetDistance() const
@@ -34,13 +67,40 @@ int Order::GetDistance() const
 }
 
 
-void Order::setStatus(ORD_STATUS s)
-{
-	status = s;
-}
-
 ORD_STATUS Order::getStatus() const
 {
 	return status;
 }
 
+
+int Order::getServTime() const {
+	return ServTime;
+}
+int Order::getFinishTime() const {
+	return FinishTime;
+}
+int Order::getWaitTime() const {
+	return WaitTime;
+}
+// int getArrivalTime() const;
+double Order::getMoney() const {
+	return totalMoney;
+}
+// price
+double Order::Getpriority() const {
+	return priority;
+}
+void Order::increamentWaitTime() {
+	WaitTime++;
+}
+
+
+
+
+
+
+
+
+Order::~Order()
+{
+}
--- Restaurant/Rest/Order.h
+++ Restaurant/Rest/Order.h
@@ -10,12 +10,10 @@ protected:
 	int ID;         //Each order has a unique ID (from 1 --> 999 )
 	ORD_TYPE type;		//order type: Normal, vegan, VIP
 	ORD_STATUS status;	//waiting, in-service, done
-	int Distance;	//The distance (in meters) between the order location and the resturant 
-	                
+	int Distance;	//The distance (in meters) between the order location and the resturant              
 	double totalMoney;	//Total order money
-
-	int ArrTime, ServTime, FinishTime;	//arrival, service start, and finish times
-	
+	int ArrTime, ServTime, FinishTime, WaitTime = 0;	//arrival, service start, and finish times
+	double priority; // added // 
 	
 	//
 	// TODO: Add More Data Members As Needed
@@ -23,21 +21,37 @@ protected:
 
 public:
 	Order(int ID, ORD_TYPE r_Type);
-	virtual ~Order();
+	Order(int ID, ORD_TYPE r_Type, double pir); //constructor for VIP orders
 
-	int GetID();
-
-	ORD_TYPE GetType() const;
-
+	//Setters // 
 	void SetDistance(int d);
-	int GetDistance() const;
-
 	void setStatus(ORD_STATUS s);
+
+	void Setpriority(double r);
+	void setWaitTime(int time);
+	void setMoney(double Cost);
+	void setArrivalTime(int Arr);
+	void setType(ORD_TYPE r_Type);
+	void setStartSrvTime(int time);
+	void setFinTime(int time);
+	void setSize(const int size);
+	// Getters //
+	int GetID();
+	ORD_TYPE GetType() const;
+	int GetDistance() const;
 	ORD_STATUS getStatus() const;
+
+	int getServTime() const;
+	int getFinishTime() const;
+	int getWaitTime() const;
+	// int getArrivalTime() const;
+	double getSize() const; // mark // 
+	double getMoney() const; // price
+	double Getpriority() const;
+	void increamentWaitTime();
+	//
+	virtual ~Order();
 	
-	//
-	// TODO: Add More Member Functions As Needed
-	//
 
 };
 
