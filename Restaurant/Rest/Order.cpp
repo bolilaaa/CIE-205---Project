19e273927db29d334cb9f2c70ed62f9b86114746
Bassam Fotouh <39791837+BFotouh@users.noreply.github.com>
2020-04-06 08:13:19 +0200

    Update Order.cpp

--- Restaurant/Rest/Order.cpp
+++ Restaurant/Rest/Order.cpp
@@ -22,10 +22,6 @@ void Order::setStatus(ORD_STATUS s)
 {
 	status = s;
 }
-void Order::SetDistance(int d)
-{
-	Distance = d > 0 ? d : 0;
-}
 
 void Order::Setpriority(double r) {
 	priority = r;
@@ -65,12 +61,6 @@ ORD_TYPE Order::GetType() const
 {
 	return type;
 }
-
-int Order::GetDistance() const
-{
-	return Distance;
-}
-
 
 ORD_STATUS Order::getStatus() const
 {
